Klozz/android_device_qcom_htc-common

Add HTC custom init support for reading props from build.prop
Reads "ro.build.fingerprint" and "ro.product.device" from /system/build.prop

* Requires following additions to Android.mk
    LOCAL_C_INCLUDES += device/qcom/htc-common/init
    LOCAL_WHOLE_STATIC_LIBRARIES += libinit_htcCommon

* Usage in init_{device}.c[pp]
    #include "init_htcCommon.h"
    and a call to: set_props_from_build(); in vendor_load_properties

* Note that if "ro.product.device" is not found in /system/build.prop
  it will be set to "ro.build.product". So be sure that is set prior
  to calling set_props_from_build()

init/Android.mk
@@ -1,0 +1,29 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := system/core/init
+LOCAL_CFLAGS := -Wall
+
+LOCAL_CFLAGS += -DPLATFORM_SDK_VERSION=$(PLATFORM_SDK_VERSION)
+
+LOCAL_SRC_FILES := init_htcCommon.cpp
+LOCAL_MODULE := libinit_htcCommon
+
+include $(BUILD_STATIC_LIBRARY)

SlimDev/device_htc_msm8960-common

    LOCAL_C_INCLUDES += system/core/init
    LOCAL_WHOLE_STATIC_LIBRARIES += libinit_msm8960
    #include "init_msm8960.h"
Change-Id: Iea16d9f1cb20bc561c67355ec17fb4221c0e8213

init/Android.mk
@@ -1,0 +1,29 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := system/core/init
+LOCAL_CFLAGS := -Wall
+
+LOCAL_CFLAGS += -DPLATFORM_SDK_VERSION=$(PLATFORM_SDK_VERSION)
+
+LOCAL_SRC_FILES := init_msm8960.cpp
+LOCAL_MODULE := libinit_msm8960
+
+include $(BUILD_STATIC_LIBRARY)